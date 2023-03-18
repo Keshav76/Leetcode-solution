62cbfeb9175d463368c71971a240c296c4109405
Keshav Banka <71920541+Keshav76@users.noreply.github.com>
2023-03-18 19:47:10 +0530

    Create

---
+++ CalendarI.cpp
@@ -0,0 +1,22 @@
+// Question: https://leetcode.com/problems/my-calendar-i/
+
+// My Calendar 1
+
+// Time Series
+
+class MyCalendar {
+public:
+    vector<pair<int, int>> arr;
+    MyCalendar() {
+        arr = {};
+    }
+    bool book(int start, int end) {
+        int i = 0;
+        while (i < arr.size() && start > arr[i].first) i ++;
+        if (i == 0 && arr.size()>0 && end > arr[0].first) return false; 
+        if (i > 0 && arr[i-1].second > start) return false;
+        if (i != 0 && i < arr.size() && arr[i].first < end) return false;
+        arr.insert(arr.begin() + i, {start, end});
+        return true;
+    }
+};
