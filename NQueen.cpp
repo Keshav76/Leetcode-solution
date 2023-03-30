2798f4189fb2221591c4f85fa53b380aaa7e3d4a
Keshav Banka <71920541+Keshav76@users.noreply.github.com>
2023-03-30 13:13:25 +0530

    Update NQueen.cpp

--- NQueen.cpp
+++ NQueen.cpp
@@ -4,63 +4,38 @@
 
 // Approach: Backtracking
 
-class Solution {
-public:
-    bool isSafe(int row,int col,vector<string> & board,int n){
-        int duprow=row;
-        int dupcol=col;
+bool isSafe(vector<string> &arr, int n, int r, int c) {
+for (int i = 0; i < n; i ++) {
+    if (arr[i][c] == 'Q' || arr[r][i] == 'Q') return false;
+}
+for (int i = 0; i < n; i ++) {
+    if (r-i >= 0 && c+i < n && arr[r-i][c+i] == 'Q') return false;
+    if (r-i >= 0 && c-i >= 0 && arr[r-i][c-i] == 'Q') return false;
+    if (r+i < n && c+i < n && arr[r+i][c+i] == 'Q') return false;
+    if (r+i < n && c-i >= 0 && arr[r+i][c+i] == 'Q') return false;
+}
+return arr[r][c] != 'Q';
+}
 
-        //case 1: upper diagonal
-        while(row>=0 && col>=0){
-            if(board[row][col]=='Q') return false;
-            row--;
-            col--;
-        }
-
-        row=duprow;
-        col=dupcol;
-
-        //case 2: same row
-        while(col>=0){
-            if(board[row][col]=='Q') return false;
-            col--;
-        }
-
-        row=duprow;
-        col=dupcol;
-
-
-        //case 3:lower diagonal
-        while(row<n && col>=0){
-            if(board[row][col]=='Q') return false;
-            row++;
-            col--;
-        }
-        return true;
+void Nqueen(int n, vector<vector<string>> &res, vector<string> &curr, int i) {
+if (i == n) {
+    res.push_back(curr);
+    return;
+}
+for (int ind = 0; ind < n; ind ++) {
+    if (isSafe(curr, n, i, ind)) {
+        curr[i][ind] = 'Q';
+        Nqueen(n, res, curr, i+1);
+        curr[i][ind] = '.';
     }
-public:
-    void solve(int col,vector<string> & board, vector<vector<string>>& ans, int n){
-        if(col==n){
-            ans.push_back(board);
-            return;
-        }
-        for(int row=0;row<n;row++){
-            if(isSafe(row,col,board,n)){
-                board[row][col]='Q';
-                solve(col+1,board,ans,n);
-                board[row][col]='.';
-            }
-        }
-    }
-  public:
-    vector < vector < string >> solveNQueens(int n) {
-      vector < vector < string >> ans;
-      vector < string > board(n);
-      string s(n, '.');
-      for (int i = 0; i < n; i++) {
-        board[i] = s;
-      }
-      solve(0, board, ans, n);
-      return ans;
-    }
-};
+}
+}
+
+vector<vector<string>> solveNQueens(int n) {
+vector<vector<string>> res;
+string t = "";
+for(int i = 0; i < n; i ++) t += '.';
+vector<string> helper (n, t);
+Nqueen(n, res, helper, 0);
+return res;
+}
