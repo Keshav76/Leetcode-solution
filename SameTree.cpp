d3e61f9e7ded92e0ea5cba9f6cafb6702830d7b8
Keshav Banka <71920541+Keshav76@users.noreply.github.com>
2023-04-22 12:58:13 +0530

    Create SameTree.cpp

---
+++ SameTree.cpp
@@ -0,0 +1,14 @@
+// Question: https://leetcode.com/problems/same-tree/
+
+// Same Tree
+
+// Approach: check left and right after checking for current
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL) return true;
+        if (p == NULL || q == NULL || p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
