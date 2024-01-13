07e7d8de41241a90dc50d8fe7d3a9b5e878ad5a9
Keshav76 <keshav.banka2003@gmail.com>
2024-01-13 12:00:00 +0530

    Add solution for LeetCode problem 3sum

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,28 +1,50 @@
-// Question: https://leetcode.com/problems/3sum
-
-// Three Sum
-
-// Approach: Sort then two pointer approach for every ele
-
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        int n = arr.size();
-        set<vector<int>> ans;
-        for (int i = 0; i < n - 2 ; i ++) {
-            int j = i + 1, k = n - 1;
-            while (j < k) {
-                if (arr[i] + arr[j] + arr[k] == 0)
-                    ans.insert({arr[i], arr[j++], arr[k--]}); 
-                else if (arr[i] + arr[j] + arr[k] > 0) k --;
-                else j ++;
-            }
-        }
-        vector<vector<int>> anss;
-        for (auto ele: ans) {
-            anss.push_back(ele);
-        }
-        return anss;
+  vector<vector<int>> threeSum(vector<int> &nums) {
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> ans;
+    vector<int> path;
+    kSum(nums, path, ans);
+    return ans;
+  }
+
+  void kSum(vector<int> &nums, vector<int> &path, vector<vector<int>> &ans,
+            int target = 0, int k = 3, int l = 0) {
+    if (k == 2) {
+      twoSum(nums, path, ans, target, l);
+    } else {
+      for (int i = l; i < nums.size(); i++) {
+        path.push_back(nums[i]);
+        kSum(nums, path, ans, target - nums[i], k - 1, i + 1);
+        path.pop_back();
+        while (i + 1 < nums.size() && nums[i] == nums[i + 1])
+          i++;
+      }
     }
+  }
+
+  void twoSum(vector<int> &nums, vector<int> &path, vector<vector<int>> &ans,
+              int target = 0, int l = 0) {
+    int r = nums.size() - 1;
+
+    while (l < r) {
+      if (nums[l] + nums[r] == target) {
+        path.push_back(nums[l]);
+        path.push_back(nums[r]);
+        ans.push_back(path);
+        path.pop_back();
+        path.pop_back();
+        while (l < r && nums[l] == nums[l + 1])
+          l++;
+        while (l < r && nums[r - 1] == nums[r])
+          r--;
+
+        l++;
+        r--;
+      } else if (nums[l] + nums[r] > target)
+        r--;
+      else
+        l++;
+    }
+  }
 };
